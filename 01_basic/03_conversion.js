e87a09a4a9a80a70d7aa0890773e97258ba73737
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-03-15 20:35:35 +0000

    operation

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,12 +1,13 @@
+// -------------------------Conversion------------------------------
 let score = "101"
 
 let changeToNum = Number(score)
 
 
-console.log( typeof score)
-console.log( typeof changeToNum)
+//console.log( typeof score)
+//console.log( typeof changeToNum)
 
-console.log(changeToNum)
+//console.log(changeToNum)
 
 
 
@@ -17,7 +18,19 @@ let score2 = "101abc"
 let changeToNum2 = Number(score2)
 
 
-console.log( typeof score2)
-console.log( typeof changeToNum2)
+//console.log( typeof score2)
+//console.log( typeof changeToNum2)
 
-console.log(changeToNum2)   // output will be NaN as score2 is not pure Number 
+//console.log(changeToNum2)   // output will be NaN as score2 is not pure Number 
+
+//------------------Operation------------------
+
+console.log("Saif " + "Ali" )
+
+console.log(1 + "3");
+
+console.log(1 + 1 + "3"); // 23
+
+console.log("1" + 1 + 3); // 113
+
+// reason is type conversion is done based on first entry
