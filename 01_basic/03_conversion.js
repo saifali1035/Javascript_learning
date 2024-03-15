667a9d7fcd652c1861ad443e78283f2c9052a194
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-03-15 20:22:19 +0000

    Datatype Conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,23 @@
+let score = "101"
+
+let changeToNum = Number(score)
+
+
+console.log( typeof score)
+console.log( typeof changeToNum)
+
+console.log(changeToNum)
+
+
+
+
+
+let score2 = "101abc"
+
+let changeToNum2 = Number(score2)
+
+
+console.log( typeof score2)
+console.log( typeof changeToNum2)
+
+console.log(changeToNum2)   // output will be NaN as score2 is not pure Number 
