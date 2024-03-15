f4c17c46cde626774815dbe3290c802ba14c172c
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-03-15 20:08:10 +0000

    variable and datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "saifali@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+let accountState
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "test.com" //allowed
+accountPass = "445566"
+accountCity = "Ranchi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+let name = "Saif" // string
+
+let age = 28 // number
+
+let isLoggedIn = false // bool
+
+null //standalone value and it is an object
+
+let test // undefined
+
+
+// object
+
+
+
+
+
+
+
+
+
+console.log(typeof isLoggedIn); //tells the type of datatype
