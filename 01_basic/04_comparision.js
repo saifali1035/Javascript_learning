e46c3f15af1620a51aedd3ee2a622ee02fd1a911
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-03-17 07:43:42 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,8 @@
+//console.log("2" > 1);
+
+console.log("3" == 3);  //came as true as equality check does conversion of the data before comparing
+
+
+console.log("3" === 3)  //false as === checks the datatype as well
+
+ 
