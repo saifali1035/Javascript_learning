6c09561d701de9e5437ffcb9151c8552b250df7c
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-03-17 08:26:18 +0000

    strings

---
+++ 01_basic/05_string.js
@@ -0,0 +1,15 @@
+name = "Saif"
+age = 28
+ console.log("Hi my name is " + name + " and my age is " + age);
+
+// string interpolation  is the new way of using variables in string
+
+console.log(`Hi my name is ${name} and my age is ${age}`);
+
+
+// another way to define string
+const newString = new String('Saif Ali')
+
+console.log(newString[0]);
+
+console.log(newString.length);
